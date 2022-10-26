08f455a222a0ba4819977fbd1f0757abd5a919ae
Kameha1011 <omarcitofc2001@gmail.com>
2022-10-26 16:46:47 -0300

    vercel deployment and minor changes to code

--- index.js
+++ index.js
@@ -2,19 +2,16 @@ const express = require('express');
 const app = express();
 // const nodemailer = require('nodemailer');
 
-app.listen(3000, () =>{
-    console.log("listening on 3000!!")
-})
+const port = process.env.PORT || 4000
+app.listen(port)
 
-const path = require('path'); //use this code to run the file from outside the container folder!!
-app.set('views', path.join(__dirname, 'views')); //this one too!
-app.use(express.static(path.join(__dirname, '/public'))) // express automatically looks for a 'public' directory where u can store static files (css, js) and connect it to your routes
+const path = require('path'); 
+app.set('views', path.join(__dirname, 'views')); 
+app.use(express.static(path.join(__dirname, '/public'))) 
 app.use(express.json())
-app.set('view engine', 'ejs'); // express automatically requires ejs (remember to create a "views" directory and a home.ejs file!)
-
-app.get('/', (req,res) => {  //app.get soporta solicitudes http del tipo get sus parametros son ('la ruta deseada',(req,res) =>{} es una funcion que toma la request y la response)
-    
-    res.render("home"); //res.render sirve para renderizar un archivo con el mismo nombre que pongamos dentro del ('') el cual sera un archivo .ejs guardado en /public
+app.set('view engine', 'ejs'); 
+app.get('/', (req,res) => {  
+    res.render("home"); 
 })
 app.get('/thanks', (req,res) => {
     res.render("thanks");
@@ -24,7 +21,3 @@ app.get('/thanks', (req,res) => {
 
 
 
-// app.post('/', (req,res) => {
-//     console.log(req.body);
-    
-// })
